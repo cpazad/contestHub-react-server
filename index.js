38b7031955bcb815a60cd9e53aad70d9dcf3b60b
cpazad <cpazad@gmail.com>
2023-11-27 04:13:23 +0600

    Contest Collection get and post route created, test, ok

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 // const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yiwkd5s.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -16,7 +15,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,34 +26,118 @@ async function run() {
     const userCollection = client.db("contestHub").collection("users");
     const contestCollection = client.db("contestHub").collection("contests");
 
-    //users Collection
-    app.get("/users",  async (req, res) => {
-        const result = await userCollection.find().toArray();
-        res.send(result);
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
       });
+      res.send({ token });
+    });
+
+    // Authetication middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        console.log(decoded);
+        req.decoded = decoded;
+
+        next();
+      });
+    };
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // Update user role endpoint verifyToken,
+    app.put("/users/updateRole/:email",  async (req, res) => {
+      const { email } = req.params;
+      const { newRole } = req.body; // Assuming you pass the new role in the request body
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      try {
+        const query = { email: email };
+        const updatedUser = await userCollection.findOneAndUpdate(
+          query,
+          { $set: { role: newRole } },
+          { new: true }
+        );
+
+        if (!updatedUser) {
+          return res.status(404).send({ message: "User not found" });
+        }
+
+        res.send({
+          message: "User role updated successfully",
+          user: updatedUser,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    //users Collection
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     // sending user data to database
     app.post("/users", async (req, res) => {
-        const user = req.body;
-        //insert email if user does not exist        
-        const query = { email: user.email };
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-          return res.send({ message: "user already exists, insertedId:null" });
-        }
-        const result = await userCollection.insertOne(user);
+      const user = req.body;
+      //insert email if user does not exist
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists, insertedId:null" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // --------------Contest Collection----------
+      // Getting the contest list
+      app.get("/contest", async (req, res) => {
+        const result = await contestCollection.find().toArray();
         res.send(result);
-      });  
+      });    
+    // Sending new contest to the database, add (verifyToken, verifyAdmin,) later
+        app.post('/contest',  async (req, res) => {
+          const item = req.body;
+          const result = await contestCollection.insertOne(item);
+          res.send(result);
+        });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // middleware
 app.use(cors());
@@ -63,9 +146,9 @@ app.use(express.json());
 // server route
 
 app.get("/", (req, res) => {
-    res.send("contestHub is in operation");
-  });
-  
-  app.listen(port, () => {
-    console.log(`ContestHub is open at port ${port}`);
-  });
+  res.send("contestHub is in operation");
+});
+
+app.listen(port, () => {
+  console.log(`ContestHub is open at port ${port}`);
+});
